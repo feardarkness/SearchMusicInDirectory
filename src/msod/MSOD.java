621201378306368108ee40272f696a64bbfe6301
ArielAlvarado <alvarado.ariel.contact@gmail.com>
2015-09-28 18:21:25 -0400

    some minor changes fix

--- src/msod/MSOD.java
+++ src/msod/MSOD.java
@@ -5,13 +5,7 @@
  */
 package msod;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Map;
 import msod.assembler.AssembleData;
-import msod.filevisitor.FileByExtensionVisitor;
 
 /**
  *
@@ -20,22 +14,8 @@ import msod.filevisitor.FileByExtensionVisitor;
 public class MSOD {
 
     public static void main(String[] args) {
-        AssembleData assembler = AssembleData("E:\\\\Musica", "C:\\Users\\aalvarado\\AppData\\Roaming\\MiniLyrics\\Lyrics");
-        Path pathToMusic = Paths.get("E:\\\\Musica");
-        Path pathToLyric = Paths.get("C:\\Users\\aalvarado\\AppData\\Roaming\\MiniLyrics\\Lyrics");
-
-        // Always send parameters on uppercase. FileByExtensionVisitor expects parameters in uppercase
-        FileByExtensionVisitor musicVisitorByType = new FileByExtensionVisitor("MP3", "MP4");
-        FileByExtensionVisitor lyricVisitorByType = new FileByExtensionVisitor("TXT", "LRC");
-        try {
-            Files.walkFileTree(pathToMusic, musicVisitorByType);
-            Files.walkFileTree(pathToLyric, lyricVisitorByType);
-            //buscarIguales(musicVisitorByType.getFileList(), lyricVisitorByType.getFileList());
-            System.out.println("Archivos de musica hallados: " + musicVisitorByType.getFileList());
-            System.out.println("Archivos de lyric hallados: " + lyricVisitorByType.getFileList());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        AssembleData assembler = new AssembleData("E:\\\\Musica", "C:\\Users\\aalvarado\\AppData\\Roaming\\MiniLyrics\\Lyrics");
+        assembler.assemble();
     }
 
 }
